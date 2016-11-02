ce6b25fa2090801017ae2650f7c8a2b9de1e410f
Remington Davenport <remington.davenport@gmail.com>
2016-11-01 23:07:27 -0400

    starting teacher

---
+++ tic-tac-toe/test/teacher_test.py
@@ -0,0 +1,22 @@
+import unittest
+import numpy
+from tictactoe.teacher import Teacher
+
+
+class TeacherTest(unittest.TestCase):
+
+    def test_converts_empty_state_to_18_zeros(self):
+        expected = numpy.array([0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
+        self.assert_converts_correctly(numpy.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]]), 1, expected)
+
+    def test_converts_only_current_player_marks_correctly(self):
+        expected = numpy.array([1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
+        self.assert_converts_correctly(numpy.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]), 1, expected)
+
+    def test_converts_only_mixed_player_marks_correctly(self):
+        expected = numpy.array([1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 1., 0., 1., 0., 0., 1., 0., 0.])
+        self.assert_converts_correctly(numpy.array([[1, 2, 0], [2, 1, 0], [2, 0, 1]]), 1, expected)
+
+    def assert_converts_correctly(self, state, player, expected):
+        result = Teacher.convert_state_to_inputs(state, player)
+        self.assertTrue(numpy.array_equal(result, expected), "expected: {}, got: {}".format(expected, result))
--- tic-tac-toe/tictactoe/teacher.py
+++ tic-tac-toe/tictactoe/teacher.py
@@ -1,4 +1,13 @@
+import numpy
+
+
 class Teacher:
-    def train(self):
+    def calculate_q_values(self, state, player):
         print('a')
+
+    @staticmethod
+    def convert_state_to_inputs(state, player):
+        current_player_state = (state.flatten() == player).astype(int)
+        other_player_state = numpy.logical_and(state.flatten() > 0, state.flatten() != player).astype(int)
+        return numpy.concatenate((current_player_state, other_player_state))
 
