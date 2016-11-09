69e38c4c93b44123c9c7df5a5c9466b629e5a79a
Remington Davenport <remington.davenport@gmail.com>
2016-11-09 08:57:25 -0500

    starting to stub out model

--- tic-tac-toe/tictactoe/teacher.py
+++ tic-tac-toe/tictactoe/teacher.py
@@ -1,4 +1,7 @@
 import numpy
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.optimizers import SGD
 
 
 class Teacher:
@@ -10,4 +13,14 @@ class Teacher:
         current_player_state = (state.flatten() == player).astype(int)
         other_player_state = numpy.logical_and(state.flatten() > 0, state.flatten() != player).astype(int)
         return numpy.concatenate((current_player_state, other_player_state))
+
+    @staticmethod
+    def create_model():
+        model = Sequential()
+        model.add(Dense(12, input_dim=18, activation='relu', init='normal'))  # hidden layer, 18 inputs
+        model.add(Dense(9, activation='sigmoid', init='normal'))  # output layer, one output for each space
+        model.summary()
+
+        optimizer = SGD(lr=0.1, momentum=0.9)
+        model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['accuracy'])
 
